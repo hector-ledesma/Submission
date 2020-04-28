93b8978c9d890c7acfcdd46a87948aa6e373cde0
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-04-28 01:03:11 -0700

    Added MainPost Tableview Cell + IBOutlets

---
+++ How-To-3/MainPostTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MainPostTableViewCell.swift
+//  How-To-3
+//
+//  Created by Bhawnish Kumar on 4/28/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class MainPostTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var postTitleLabel: UILabel!
+    @IBOutlet weak var likesLabel: UILabel!
+    @IBOutlet weak var authorNameLabel: UILabel!
+    @IBOutlet weak var timeStampLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
