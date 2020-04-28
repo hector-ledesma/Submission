d9c2f7202489e8184fe57ca23f98be40468a6d4d
Hector L <hectoremil21@hotmail.com>
2020-04-28 10:41:14 -0400

    No real changes. Lots of testing to try and fix swiftlint

--- How-To-3/BackendController.swift
+++ How-To-3/BackendController.swift
@@ -14,8 +14,9 @@ class BackendController {
     var dataLoader: DataLoader?
 
     // If the initializer isn't provided with a data loader, simply use the URLSession singleton.
-    init(dataLoader: DataLoader = URLSession.shared) {
+    init(dataLoader: DataLoader = URLSession.shared, data: Data?) {
         self.dataLoader = dataLoader
+        self.data = data
     }
 
     //func signUp(username: String, password: String, email: String) {
@@ -23,9 +24,9 @@ class BackendController {
      //   let request = URLRequest(url: baseURL)
      //   dataLoader?.loadData(from: <#T##URLRequest#>, completion: <#T##(Data?, Error?) -> Void#>)
     //}
-
+    let data: Data?
     func signIn() {
-
+//        let foo = try! JSONDecoder().decode(UserRepresentation.self, from: data!)
     }
 
     private enum EndPoints: String {
